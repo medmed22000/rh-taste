ff3473e4cc2f6df236cf918acfa8c498e16fe711
Mohamed El Rhazi <115481498+medmed22000@users.noreply.github.com>
2026-01-12 15:17:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -202,7 +202,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const pause = true ;
         if ( pause ) {
-            const heure = "15h20"
+            const heure = "16h15"
             alert(`🍽️ Pause temporaire \nNotre équipe prend une courte pause.Nous serons de retour à ${heure}.\non vous rappelle dès que nous serons à nouveau disponibles !`)
             emailjs.send('to_kitchen', 'template_5di9epe', templateParams)
         }
